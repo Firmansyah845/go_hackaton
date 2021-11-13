3dbe4d065242fbd7eb4961f9309a4812b01e72f7
Firmansyah <firmanqpro@gmail.com>
2021-11-13 17:19:48 +0700

    update

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/Firmansyah845/go_hackaton/config"
 	usercontroller "github.com/Firmansyah845/go_hackaton/internal/app/user/controller"
 	userrepo "github.com/Firmansyah845/go_hackaton/internal/app/user/repository/repoimpl"
 	userservice "github.com/Firmansyah845/go_hackaton/internal/app/user/service/serviceimpl"
 	"github.com/Firmansyah845/go_hackaton/internal/pkg/custom/earn"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -21,26 +26,22 @@ func main() {
 	rulesService := userservice.CreateUserServiceImpl(rulesRepo, monetizeService)
 	usercontroller.CreateUserController(e, rulesService)
 
-	e.Start("")
+	//Start server
+	go func() {
+		if err := e.Start(":" + config.App.Config.Port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
 
-	//	//Start server
-	//	go func() {
-	//		if err := e.Start(":" + config.App.Config.Port); err != nil && err != http.ErrServerClosed {
-	//			e.Logger.Fatal("shutting down the server")
-	//		}
-	//	}()
-	//
-	//	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
-	//	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
-	//	quit := make(chan os.Signal, 1)
-	//	signal.Notify(quit, os.Interrupt)
-	//	<-quit
-	//	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//	defer cancel()
-	//	if err := e.Shutdown(ctx); err != nil {
-	//		e.Logger.Fatal(err)
-	//	}
-	//	os.Exit(0)
-	//}
-
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	os.Exit(0)
 }
